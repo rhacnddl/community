825be240f079d619468dffa5246d9066b03c03b3
rhacnddl <jiho519@naver.com>
2021-04-02 03:43:35 +0900

    2021/04/02 WebSocket Apply

--- src/main/java/org/gorany/community/config/WebSocketConfig.java
+++ src/main/java/org/gorany/community/config/WebSocketConfig.java
@@ -16,6 +16,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatHandler, "/chat").setAllowedOrigins("*");
+        registry.addHandler(chatHandler, "/ws/chat").setAllowedOrigins("*");
     }
 }
--- src/main/java/org/gorany/community/controller/ChatController.java
+++ src/main/java/org/gorany/community/controller/ChatController.java
@@ -1,7 +1,10 @@
 package org.gorany.community.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.gorany.community.dto.MemberDTO;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
